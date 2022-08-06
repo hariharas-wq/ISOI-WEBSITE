be5fa8ab7525b97c5545070d6bdeb2a2116a1369
HB7723 <hb772325@gmail.com>
2022-08-06 23:14:04 +0530

    tailwind setup completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,18 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        heading: ["'Cinzel', serif"],
+        primary: ["'Quattrocento Sans', sans-serif"],
+        navbar: ["'Inter', sans-serif"],
+      },
+      colors: {
+        primary: "#293855",
+        secondary: "#4165D5",
+        copyright: "rgba(65, 101, 213, 0.62);",
+      },
+    },
   },
   plugins: [],
 };
